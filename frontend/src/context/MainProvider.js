f84339449b8e336a5c9ea6b98ca24d44ba34ead8
thosijulio <thosijulio@gmail.com>
2021-12-13 15:49:35 -0300

    inicio do front; criacao do context e do provider

---
+++ frontend/src/context/MainProvider.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import MainContext from './MainContext';
+
+
+const MainProvider = ({ children }) => {
+  const [city, setCity] = useState();
+  const [cities, setCities] = useState();
+  const [events, setEvents] = useState();
+
+  return (
+    <MainContext.Provider value={ { city, setCity, cities, setCities, events, setEvents } }>
+      { children }
+    </MainContext.Provider>
+  )
+};
+
+export default MainProvider;
