e7e719f8be4db2e1567cf7307cc10f9029a8bf22
BSY-Development <bes.yamamoto@gmail.com>
2021-12-14 00:27:08 -0300

    Removido linha duplicada do index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const bodyParser = require('body-parser');
-const cors = require('cors')
 
 const CitiesRouter = require('./routes/CitiesRouter');
 const EventsRouter = require('./routes/EventsRouter');
